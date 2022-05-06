30e05d7cefcac0d19c35d5203964e8fc0b151bd0
FrostIce482 <frostice48260@gmail.com>
2022-05-06 11:50:16 +0700

    permission module

---
+++ dev/src/libcore/permission.ts
@@ -0,0 +1,30 @@
+import { empty } from "./misc.js";
+
+const list: List<number> = empty()
+
+export default class permission {
+    /**
+     * Assigns permission level to a tag.
+     * @param tag Tag which permission level will be assigned to.
+     * @param level Permission level that will be assigned to the tag.
+     */
+    static readonly assign = (tag: string, level: number) => ( list[tag] = level, this )
+
+    /**
+     * Test if a tag has permission level assigned to it.
+     * @param tag Tag.
+     */
+    static readonly isAssigned = (tag: string) => tag in list
+
+    /**
+     * Deassigns permission level from a tag.
+     * @param tag Tag which permission level will be removed.
+     */
+    static readonly deassign = (tag: string) => { const v = tag in list; return delete list[tag], v }
+
+    /**
+     * Gets permission level from tags.
+     * @param tags Tags.
+     */
+    static readonly getLevel = (tags: string[]) => tags.reduce((lev, tag) => ( tag in list && list[tag] > lev ) ? list[tag] : lev, 0)
+}
