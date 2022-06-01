676749a5213edbbabe4d667e3dbe53317ca9fd2f
FrostIce482 <frostice48260@gmail.com>
2022-06-01 13:08:06 +0700

    delete storage

--- dev/src/defaultcc/eval.ts
+++ dev/src/defaultcc/eval.ts
@@ -10,7 +10,7 @@ import plr from "../libcore/plr.js";
 import scoreboard from "../libcore/scoreboard.js";
 import * as sendChat from "../libcore/sendChat.js";
 import { sendMsg } from "../libcore/sendChat.js";
-import storage from "../libcore/storage.js";
+//import storage from "../libcore/storage.js";
 import server from "../libcore/server.js";
 import TypedValues from "../libcore/typedvalues.js";
 
@@ -35,7 +35,7 @@ const o = new Proxy({
     sendChat,
     sendMsg,
     server,
-    storage,
+    //storage,
     TypedValues,
     get executer() { return  gExecuter },
     get evd() { return  gEvd },
