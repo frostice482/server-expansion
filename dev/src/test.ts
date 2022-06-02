921cfb4b960101f0f7bb9467bb1c2b2a43758e05
FrostIce482 <frostice48260@gmail.com>
2022-06-02 20:15:02 +0700

    dummy spawn

--- dev/src/test.ts
+++ dev/src/test.ts
@@ -1,14 +1,20 @@
-import { world } from "mojang-minecraft"
+import { BlockLocation, world } from "mojang-minecraft"
+import * as gt from 'mojang-gametest'
 import server from "./libcore/server.js"
-import storage from "./libcore/storage.js"
 
 world.events.tick.subscribe(({deltaTime}) => {
     for (const plr of world.getPlayers())
         plr.onScreenDisplay.setActionBar(`delta: ${(deltaTime * 1000).toFixed(2)}ms (tps: ${(1/deltaTime).toFixed(2)}) servertime: ${server.time}ms tickertime: ${server.ticker.time}ms (type: ${server.ticker.level})`)
 })
 
-storage.instance.default.ev.save.subscribe(data => console.warn('storage save initialized'), 1000)
-storage.instance.default.ev.postSave.subscribe(data => console.warn(`storage save done, time: ${data.time}, size: ${data.stringed.length}`), 1000)
+gt.register("SE", "spawnFakePlayer", (test) => {
+    test.print('A simulated player has been spawned. It will exist for up to 120 seconds.')
 
-storage.instance.default.ev.load.subscribe(data => console.warn('storage load initialized'), 1000)
-storage.instance.default.ev.postLoad.subscribe(data => console.warn(`storage load done, time: ${data.time}, size: ${data.stringed.length}`), 1000)
+    const spawnLoc = new BlockLocation(1, 2, 1)
+    let player = test.spawnSimulatedPlayer(spawnLoc, 'Dummy');
+
+    test.succeedWhen(() => test.assertEntityPresentInArea('player', false))
+    test.succeedOnTick(2400)
+})
+    .maxTicks(2420)
+    .structureName("ComponentTests:platform")
