84b936b9402473d1937f7d5c79b30d74fd343484
FrostIce482 <frostice48260@gmail.com>
2022-06-12 20:02:59 +0700

    update test

--- dev/src/test.ts
+++ dev/src/test.ts
@@ -2,11 +2,6 @@ import { BlockLocation, world } from "mojang-minecraft"
 import * as gt from 'mojang-gametest'
 import server from "./libcore/server.js"
 
-world.events.tick.subscribe(({deltaTime}) => {
-    for (const plr of world.getPlayers())
-        plr.onScreenDisplay.setActionBar(`delta: ${(deltaTime * 1000).toFixed(2)}ms (tps: ${(1/deltaTime).toFixed(2)}) servertime: ${server.time}ms tickertime: ${server.ticker.time}ms (type: ${server.ticker.level})`)
-})
-
 gt.register("SE", "spawnFakePlayer", (test) => {
     test.print('A simulated player has been spawned. It will exist for up to 120 seconds.')
 
