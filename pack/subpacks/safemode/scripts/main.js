64e21b63b05b4a21602e9b4c2ce342e24ab5c1b9
FrostIce482 <frostice48260@gmail.com>
2022-06-16 19:22:36 +0700

    fix safemode not working

--- pack/subpacks/safemode/scripts/main.js
+++ pack/subpacks/safemode/scripts/main.js
@@ -1,3 +1,16 @@
 import storage from './libcore/storage.js';
 storage.instance.default.autoload = false
 storage.instance.default.autosaveInterval = 0
+
+import server from './libcore/server.js'
+server.start()
+server.ev.beforeChat.subscribe((evd) => {
+    if (evd.cancel) return
+    evd.cancel = true
+    if (evd.message.startsWith(cc.prefix)) {
+        cc.execute(evd)
+        return
+    } else {
+        chat.send(evd.sender, evd.message)
+    }
+}, 0)
