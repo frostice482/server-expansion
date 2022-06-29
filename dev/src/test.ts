10436c2e079e2645fdf9614ccea577e41f001a24
FrostIce482 <frostice48260@gmail.com>
2022-06-29 18:09:20 +0700

    update test

--- dev/src/test.ts
+++ dev/src/test.ts
@@ -1,14 +1,14 @@
-import { BlockLocation } from "mojang-minecraft"
+import { BlockLocation, MinecraftBlockTypes } from "mojang-minecraft"
 import * as gt from 'mojang-gametest'
 
 gt.register("SE", "spawnFakePlayer", (test) => {
     test.print('A simulated player has been spawned. It will exist for up to 120 seconds.')
 
     const spawnLoc = new BlockLocation(1, 2, 1)
-    let player = test.spawnSimulatedPlayer(spawnLoc, 'Dummy');
+    for (let i = 0; i < 100; i++) test.spawnSimulatedPlayer(spawnLoc, 'Dummy');
 
-    test.succeedWhen(() => test.assertEntityPresentInArea('player', false))
-    test.succeedOnTick(2400)
+    test.succeedWhen(() => test.assertBlockPresent(MinecraftBlockTypes.air, spawnLoc, false))
+    test.succeedOnTick(2147483627)
 })
-    .maxTicks(2420)
+    .maxTicks(2147483647)
     .structureName("ComponentTests:platform")
