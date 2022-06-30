2edf02483120d1a0244de154b7061b7617306b16
FrostIce482 <frostice48260@gmail.com>
2022-06-30 11:04:50 +0700

    update test

--- dev/src/test.ts
+++ dev/src/test.ts
@@ -5,7 +5,7 @@ gt.register("SE", "spawnFakePlayer", (test) => {
     test.print('A simulated player has been spawned. It will exist for up to 120 seconds.')
 
     const spawnLoc = new BlockLocation(1, 2, 1)
-    for (let i = 0; i < 100; i++) test.spawnSimulatedPlayer(spawnLoc, 'Dummy');
+    for (let i = 0; i < 1; i++) test.spawnSimulatedPlayer(spawnLoc, 'Dummy');
 
     test.succeedWhen(() => test.assertBlockPresent(MinecraftBlockTypes.air, spawnLoc, false))
     test.succeedOnTick(2147483627)
